ab86f2d591e9c2e1e7a396c80c0914815b42aabc
Aleksei2576 <118676730+Aleksei2576@users.noreply.github.com>
2023-07-18 05:36:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.put('/image', (req, res) => {
 	.catch(err => res.status(400).json('Not found'))
 })
 
-app.post('/put', (req, res) => {
+app.post('/box', (req, res) => {
 	const {input} = req.body;
 	fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnFaceBox(input))
     .then(response => response.json())
